32d5f7fe86ce5e120609b2fb609930ef078d96ee
zonika <zchodosh@conncoll.edu>
2015-08-03 15:08:13 -0400

    done

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
+  def keys_of(*values)
     # code goes here
+    matched=[]
+    self.each do |key, value|
+      values.each do |val|
+        if val==value
+          matched.push(key)
+        end
+      end
+    end
+    matched
   end
 end
